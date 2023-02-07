cb7ddfbb772e5b7a0d42d3ff194bd243c3fc73ec
Almaylastoscas <alitocassan@gmail.com>
2023-02-07 19:50:23 -0300

    Ajustes para la entrega

--- ecommercereact/src/components/Navbar.jsx
+++ ecommercereact/src/components/Navbar.jsx
@@ -7,13 +7,12 @@ const Navbar = () => {
     <nav class="bg-gray-800">
       <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
         <div class="relative flex h-16 items-center justify-between">
-          <div class="absolute inset-y-0 left-0 flex items-center sm:hidden"></div>
-          <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-            <div class="flex flex-shrink-0 items-center">
-              <img class="hidden h-8 w-auto lg:block" src="./logo-almay.png" />
-            </div>
-            <div class="hidden sm:ml-6 sm:block">
-              <div class="flex  space-x-4 ">
+          <a href="#">
+            <img class="hidden h-8 w-auto lg:block" src="./logo-almay.png" />
+          </a>
+          <div class="flex flex-1 items-center content-center justify-center sm:items-stretch   ">
+            <div class="hidden sm:ml-6  sm:block">
+              <div class="flex  space-x-6 ">
                 <Categories />
               </div>
             </div>
